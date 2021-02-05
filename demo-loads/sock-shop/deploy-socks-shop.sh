ec685307b2be142f53e2c568a958ff15129276ea
martijnvdp <m.vanderploeg@nl.ccv.eu>
2021-02-05 11:45:54 +0100

    sript root path

--- demo-loads/sock-shop/deploy-socks-shop.sh
+++ demo-loads/sock-shop/deploy-socks-shop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+SR="`dirname \"$0\"`"
 kubectl create namespace sock-shop
-kubectl apply -f ./complete-sock-shop.yaml
-kubectl apply -f ./istio-sockshop-gateway.yaml -n sock-shop
-kubectl apply -f ./istio-sockshop-virtualservices.yaml -n sock-shop
+kubectl apply -f $SR/complete-sock-shop.yaml
+kubectl apply -f $SR/istio-sockshop-gateway.yaml -n sock-shop
+kubectl apply -f $SR/istio-sockshop-virtualservices.yaml -n sock-shop
